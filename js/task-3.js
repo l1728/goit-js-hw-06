01cff1d543dd1bd3aa82b7691a651ad90cad5eb6
l1728 <cheburashka077@ukr.net>
2024-02-28 23:34:17 +0200

    done

--- js/task-3.js
+++ js/task-3.js
@@ -1,21 +1,26 @@
-const profile = {
-    username: "Jacob",
-    playTime: 300,
-    changeUsername(newName) {
-        this.username = newName;
-    },
-    updatePlayTime(hours) {
-        this.playTime += hours;
-    },
-    getInfo() {
-        return `${this.username} has ${this.playTime} active hours!`
+class StringBuilder {
+    #value;
+    constructor(initialValue = " ") {
+        this.#value = initialValue.toString();
     }
-};
-
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
-
-profile.changeUsername("Marco");
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
-
-profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"
+    getValue() {
+        return this.#value;
+    }
+    padEnd(str) {
+        this.#value += str.toString();
+    }
+    padStart(str) {
+        this.#value = str.toString() + this.#value;
+    }
+    padBoth(str) {
+        this.#value = str.toString() + this.#value + str.toString();
+    }
+}
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
